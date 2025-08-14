6b7fa87c0173d951a7058618e6c9ae13fe69ccc7
tantos <dek.tantos@gmail.com>
2025-08-14 10:52:34 +0700

    sip

--- server/app/Config/Database.php
+++ server/app/Config/Database.php
@@ -27,9 +27,9 @@ class Database extends Config
     public array $default = [
         'DSN'          => '',
         'hostname'     => 'localhost',
-        'username'     => '',
-        'password'     => '',
-        'database'     => '',
+        'username'     => 'servermanager',
+        'password'     => 'b15m1l4h',
+        'database'     => 'servermanager',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
@@ -41,7 +41,7 @@ class Database extends Config
         'compress'     => false,
         'strictOn'     => false,
         'failover'     => [],
-        'port'         => 3306,
+        'port'         => 33066,
         'numberNative' => false,
         'foundRows'    => false,
         'dateFormat'   => [
